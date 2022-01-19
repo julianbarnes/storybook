57dfd0897a8b0443fb31413093aefb6dc110c792
Julian <julian.barnes100@gmail.com>
2022-01-19 16:50:25 -0500

    Adds the next button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -11,25 +14,37 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.componentInstance
+    });
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'storyboard'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('storyboard');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('storyboard app is running!');
-  });
+
+  it('should go to the next page when the right button is clicked', fakeAsync(() => {
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button')
+
+    button?.click();
+    tick();
+    fixture.detectChanges()
+    let content = (fixture.debugElement.nativeElement as HTMLElement).textContent
+    console.log(content)
+    expect(content?.indexOf('Page 2')).toBeGreaterThan(0)
+    // fixture.whenStable().then(() => {
+    //   expect((fixture.debugElement.nativeElement as HTMLElement).textContent?.indexOf('Page 2')).toBeGreaterThan(0)
+    // })
+    //click 
+  }))
+
+  it('should go to the previous page when the left button is clicked')
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'storyboard';
+
+  stories = [{content: 'This is the beginning of the story', page: 1}, {content: 'What happens next is up to you ', page: 2}]
+
+  currentPage = 1
+
+  button: HTMLElement | null = null;
+  onClick() {
+    this.currentPage++;
+  }
+
+
 }
